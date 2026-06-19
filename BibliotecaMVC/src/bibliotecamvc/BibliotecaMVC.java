9b86c9c00ef5553270db0aa45eace0ae0ac4e72a
noemiguapi0393-collab <noemi.guapi0393@utc.edu.ec>
2026-06-18 21:41:49 -0500

    docs: documentado el bucle principal do-while en el main

--- BibliotecaMVC/src/bibliotecamvc/BibliotecaMVC.java
+++ BibliotecaMVC/src/bibliotecamvc/BibliotecaMVC.java
@@ -4,43 +4,34 @@
  */
 package bibliotecamvc;
 
-// Importamos la vista para que el Main pueda usar el menú
 import BibliotecaView.MenuPrincipal;
 
 public class BibliotecaMVC {
 
     public static void main(String[] args) {
-        // Creamos el objeto del menú interactivo
         MenuPrincipal vista = new MenuPrincipal();
-        
         int opcion;
 
-        // Bucle repetitivo para que el menú no se cierre solo
+        // Bucle principal que mantiene el programa ejecutándose hasta elegir salir
         do {
             opcion = vista.mostrarMenu();
             
-            // Switch-case para evaluar la opción marcada
             switch (opcion) {
                 case 1:
                     System.out.println("\n[Acción]: Has seleccionado Registrar Libro.");
                     break;
-                    
                 case 2:
                     System.out.println("\n[Acción]: Has seleccionado Registrar Usuario.");
                     break;
-                    
                 case 3:
                     System.out.println("\n[Acción]: Has seleccionado Registrar Préstamo.");
                     break;
-                    
                 case 4:
                     System.out.println("\n[Acción]: Has seleccionado Mostrar Reportes.");
                     break;
-                    
                 case 5:
                     System.out.println("\nSaliendo del sistema de biblioteca... ¡Hasta luego!");
                     break;
-                    
                 default:
                     System.out.println("\n[ERROR]: Opción no válida. Intenta de nuevo.");
             }
